ac2107bf56beae7e5a15199e2302f6dce28f9eae
bxffysxmmers <warricrsmind@gmail.com>
2025-11-19 00:32:46 -0800

    Add Google deployment and sheet IDs

---
+++ source/js/settings.js
@@ -0,0 +1,7 @@
+const deployID = 'AKfycbw-J6UK1cM4dyrj5vdD0BUQvKFW8zrsL8yCAm0X7aI75_rWvCxl3XcnfZ5Jd8y_e6Yy-g';
+const sheetID = `143m8zqqfdHnf9g1nN5haTwvzbD-f-8Q5dRW8o-ElA44`;
+const oldSheetID = ``;
+const successMessage = `<p class="fullWidth">Submission successful!</p>
+<button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
+const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
+const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
